1ba69df92b39523a5e064c945a75f944ecad485f
Mohd Altaaf <mohdaltaaf24@gmail.com>
2026-07-24 17:40:39 +0530

    camera movement done!

---
+++ Assets/Script/movementScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movementScript : MonoBehaviour
+{
+    private Camera playerCamera;
+
+    public float MouseSensitivity = 0.1f;
+    public float minPitch = -85f;
+    public float maxPitch = 85f;
+    private Vector2 MouseInput;
+    private float mouseHorizontal;
+    private float mouseVertical;
+    private float Xrotation;
+    private float Yrotation;
+
+
+    void OnLook(InputValue Value)
+    {
+        MouseInput = Value.Get<Vector2>();
+        mouseHorizontal = MouseInput.x;
+        mouseVertical = MouseInput.y;
+        
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Xrotation -= Mathf.Clamp(mouseVertical*MouseSensitivity, minPitch, maxPitch);
+        Yrotation += mouseHorizontal*MouseSensitivity;
+        Quaternion desiredRotation = Quaternion.Euler(Xrotation, Yrotation, 0);
+        transform.localRotation = desiredRotation;
+
+    }
+}
